ede0585d7851968b1778339e6054dcce3ebef22c
ysaadaou <ysaadaou@gmail.com>
2026-03-10 08:04:13 -0400

    sessionMarch10

--- script.js
+++ script.js
@@ -48,10 +48,12 @@ const students = [
 
 const displayStudents = document.getElementById("displayStudents");
 
-students.forEach((student) => {
-  let box = document.createElement("div");
-  box.classList.add("box");
-  box.innerHTML = `
+function render() {
+  displayStudents.innerHTML = "";
+  students.forEach((student) => {
+    let box = document.createElement("div");
+    box.classList.add("box");
+    box.innerHTML = `
         <img class="pic" src=${student.pic} alt="student picture" />
         <h2>${student.nom} ${student.prenom} </h2>
         <div class="contactInfo">
@@ -59,17 +61,79 @@ students.forEach((student) => {
         <a href="tel:${student.telephone}">Phone</a>
         </div>
 
-        <button class="editIcon" onclick="edit()">
-        <svg   xmlns="http://www.w3.org/2000/svg" width="30" height="30"  
-fill="currentColor" viewBox="0 0 24 24" >
-<!--Boxicons v3.0.8 https://boxicons.com | License  https://docs.boxicons.com/free-->
-<path d="M5 21h14c1.1 0 2-.9 2-2v-7h-2v7H5V5h7V3H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2"></path><path d="M7 13v3c0 .55.45 1 1 1h3c.27 0 .52-.11.71-.29l9-9a.996.996 0 0 0 0-1.41l-3-3a.996.996 0 0 0-1.41 0l-9.01 8.99A1 1 0 0 0 7 13m10-7.59L18.59 7 17.5 8.09 15.91 6.5zm-8 8 5.5-5.5 1.59 1.59-5.5 5.5H9z"></path>
-</svg>
-        </button>
 
     `;
 
-  displayStudents.appendChild(box);
-});
+    //         <button class="editIcon" onclick="edit()">
+    //         <svg   xmlns="http://www.w3.org/2000/svg" width="30" height="30"
+    // fill="currentColor" viewBox="0 0 24 24" >
+    // <!--Boxicons v3.0.8 https://boxicons.com | License  https://docs.boxicons.com/free-->
+    // <path d="M5 21h14c1.1 0 2-.9 2-2v-7h-2v7H5V5h7V3H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2"></path><path d="M7 13v3c0 .55.45 1 1 1h3c.27 0 .52-.11.71-.29l9-9a.996.996 0 0 0 0-1.41l-3-3a.996.996 0 0 0-1.41 0l-9.01 8.99A1 1 0 0 0 7 13m10-7.59L18.59 7 17.5 8.09 15.91 6.5zm-8 8 5.5-5.5 1.59 1.59-5.5 5.5H9z"></path>
+    // </svg>
+    //         </button>
+    const btn = document.createElement("button");
+    btn.textContent = "Edit";
+    btn.classList.add("editIcon");
+    btn.addEventListener("click", () => edit(student));
 
-function edit() {}
+    box.appendChild(btn);
+    displayStudents.appendChild(box);
+  });
+}
+
+render();
+function edit(student) {
+  const overlay = document.createElement("div");
+  overlay.classList.add("overlay");
+  overlay.id = "overlay";
+  const popup = document.createElement("div");
+  popup.classList.add("popup");
+  const closeBtn = document.createElement("button");
+  closeBtn.classList.add("close");
+  closeBtn.textContent = "X";
+  closeBtn.addEventListener("click", handleClose);
+  popup.innerHTML = `
+  <form id="studentForm"  class="studentInfo">
+  <label>
+    Nom: <input type="text" name="nom" value=${student.nom} >
+  </label>
+  <label>
+    Prenom: <input type="text" name="prenom" value=${student.prenom}>
+  </label>
+    <div>email: <input type="text" name="email" value=${student.email}></div>
+    <label>telephone: <input type="text" name="telephone" value=${student.telephone}></label>
+
+    </form>
+
+  `;
+  let saveBtn = document.createElement("button");
+  saveBtn.textContent = "Save";
+  saveBtn.addEventListener("click", (e) => {
+    save(e, student);
+  });
+  popup.appendChild(closeBtn);
+  popup.appendChild(saveBtn);
+  overlay.appendChild(popup);
+  document.body.append(overlay);
+}
+
+function handleClose(e) {
+  e.target.parentElement.parentElement.remove();
+}
+
+function save(e, student) {
+  console.log("saved");
+  let form = e.target.parentElement.querySelector("#studentForm");
+
+  // let studentToUpdate = students.find((s) => s.id === student.id);
+  // console.log(studentToUpdate);
+
+  for (let i = 0; i < form.length; i++) {
+    let key = form[i].name;
+    let value = form[i].value;
+    student[key] = value;
+  }
+
+  render();
+  document.getElementById("overlay").remove();
+}
