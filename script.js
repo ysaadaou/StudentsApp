a8076f425ec39ec9c23eb410dd90fd9612ba26b1
ysaadaou <ysaadaou@gmail.com>
2026-03-24 04:10:48 -0400

    new features: add student

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const students = [
+let students = [
   {
     id: "F132156",
     cin: "EE458721",
@@ -60,29 +60,49 @@ function render() {
         <a href="mailto:${student.email}">Send email </a>
         <a href="tel:${student.telephone}">Phone</a>
         </div>
-
-
     `;
 
-    //         <button class="editIcon" onclick="edit()">
-    //         <svg   xmlns="http://www.w3.org/2000/svg" width="30" height="30"
-    // fill="currentColor" viewBox="0 0 24 24" >
-    // <!--Boxicons v3.0.8 https://boxicons.com | License  https://docs.boxicons.com/free-->
-    // <path d="M5 21h14c1.1 0 2-.9 2-2v-7h-2v7H5V5h7V3H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2"></path><path d="M7 13v3c0 .55.45 1 1 1h3c.27 0 .52-.11.71-.29l9-9a.996.996 0 0 0 0-1.41l-3-3a.996.996 0 0 0-1.41 0l-9.01 8.99A1 1 0 0 0 7 13m10-7.59L18.59 7 17.5 8.09 15.91 6.5zm-8 8 5.5-5.5 1.59 1.59-5.5 5.5H9z"></path>
-    // </svg>
-    //         </button>
-    const btn = document.createElement("button");
-    btn.textContent = "Edit";
-    btn.classList.add("editIcon");
-    btn.addEventListener("click", () => edit(student));
+    const editBtn = document.createElement("button");
+    editBtn.textContent = "Edit";
+    editBtn.classList.add("editBtn");
+    editBtn.addEventListener("click", () => edit(student));
+    box.appendChild(editBtn);
 
-    box.appendChild(btn);
+    const deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "Supprimer";
+    deleteBtn.classList.add("deletBtn");
+    deleteBtn.addEventListener("click", () => deleteStudent(student));
+    box.appendChild(deleteBtn);
+
     displayStudents.appendChild(box);
   });
+
+  let box = document.createElement("div");
+  box.classList.add("box");
+  box.innerHTML = `
+    <button class="addBtn" onclick="addStudent()">Add</button>
+  `;
+  displayStudents.appendChild(box);
 }
 
 render();
+
+function deleteStudent(student) {
+  students = students.filter((std) => {
+    return std.id != student.id;
+  });
+  render();
+}
+
 function edit(student) {
+  general(student);
+}
+
+function addStudent() {
+  general();
+}
+
+function general(student) {
   const overlay = document.createElement("div");
   overlay.classList.add("overlay");
   overlay.id = "overlay";
@@ -94,14 +114,18 @@ function edit(student) {
   closeBtn.addEventListener("click", handleClose);
   popup.innerHTML = `
   <form id="studentForm"  class="studentInfo">
+
   <label>
-    Nom: <input type="text" name="nom" value=${student.nom} >
+    Id: <input type="text" name="id" value=${student != undefined ? student.id : ""} >
   </label>
   <label>
-    Prenom: <input type="text" name="prenom" value=${student.prenom}>
+    Nom: <input type="text" name="nom" value=${student != undefined ? student.nom : ""} >
   </label>
-    <div>email: <input type="text" name="email" value=${student.email}></div>
-    <label>telephone: <input type="text" name="telephone" value=${student.telephone}></label>
+  <label>
+Prenom: <input type="text" name="prenom" value=${student != undefined ? student.prenom : ""}>
+  </label>
+    <div>email: <input type="text" name="email" value=${student != undefined ? student.email : ""}></div>
+    <label>telephone: <input type="text" name="telephone" value=${student != undefined ? student.telephone : ""}></label>
 
     </form>
 
@@ -122,16 +146,23 @@ function handleClose(e) {
 }
 
 function save(e, student) {
-  console.log("saved");
   let form = e.target.parentElement.querySelector("#studentForm");
 
-  // let studentToUpdate = students.find((s) => s.id === student.id);
-  // console.log(studentToUpdate);
-
-  for (let i = 0; i < form.length; i++) {
-    let key = form[i].name;
-    let value = form[i].value;
-    student[key] = value;
+  if (student != undefined) {
+    for (let i = 0; i < form.length; i++) {
+      let key = form[i].name;
+      let value = form[i].value;
+      student[key] = value;
+    }
+  } else {
+    let newStudent = {};
+    for (let i = 0; i < form.length; i++) {
+      let key = form[i].name;
+      let value = form[i].value;
+      newStudent[key] = value;
+    }
+    newStudent.pic = "pics/defaultProfilePic.png";
+    students.push(newStudent);
   }
 
   render();
